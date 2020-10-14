f22f74ffe625f7205174c7f3b52adcefc7b41a58
Dario Kolic <coachbuilds@gmail.com>
2020-10-14 23:39:46 +0200

    Updates

---
+++ precache-manifest.cce8ffcbeaaa931c4d01f4db53174386.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c4d81f8a66ce8ad30303e9a383efda",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89c5c6c46479a1fe5cc4",
+    "url": "/portfolio/static/css/2.89c4d570.chunk.css"
+  },
+  {
+    "revision": "95f51a083b1b0fd1beba",
+    "url": "/portfolio/static/css/main.294e2f60.chunk.css"
+  },
+  {
+    "revision": "89c5c6c46479a1fe5cc4",
+    "url": "/portfolio/static/js/2.5ba1e6ba.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/portfolio/static/js/2.5ba1e6ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f51a083b1b0fd1beba",
+    "url": "/portfolio/static/js/main.cedf8387.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/portfolio/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/portfolio/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "1362fca334bbc5059ad8242bf7e1664d",
+    "url": "/portfolio/static/media/eos-icons.1362fca3.ttf"
+  },
+  {
+    "revision": "1c92b0e331c4e4cc6fabd25758f15146",
+    "url": "/portfolio/static/media/eos-icons.1c92b0e3.svg"
+  },
+  {
+    "revision": "a4115e59f2575bafc24e8ad624aad409",
+    "url": "/portfolio/static/media/eos-icons.a4115e59.eot"
+  },
+  {
+    "revision": "dacccdca2ab41e5f05343933aefd5804",
+    "url": "/portfolio/static/media/eos-icons.dacccdca.woff2"
+  },
+  {
+    "revision": "e2fb078ed59325bdd03d86554cba70c7",
+    "url": "/portfolio/static/media/eos-icons.e2fb078e.woff"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/portfolio/static/media/firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "64d5e98f1a220cdcad2b1a70d52ab669",
+    "url": "/portfolio/static/media/git-icon.64d5e98f.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/portfolio/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/portfolio/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "61fdea8bade15ac78d711eb0fc1f9d81",
+    "url": "/portfolio/static/media/logo.61fdea8b.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/portfolio/static/media/nodejs.f434bfbb.svg"
+  },
+  {
+    "revision": "abffe26916211a4f0f66cade774cd6db",
+    "url": "/portfolio/static/media/profile.abffe269.jpg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/portfolio/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/portfolio/static/media/redux.85772647.svg"
+  }
+]);
